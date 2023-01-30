136b9b1a0b3b4c32a7fbb0b369ae053130a72aa7
Erik Basañez <basanez_erik@eitb.eus>
2023-01-30 16:26:18 +0100

    yeah

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'dart:io';
  
 void main() {
   runApp(const MyApp());
@@ -21,9 +24,72 @@ class MyApp extends StatelessWidget {
  
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
+
+  Map _getVias() {
+    File vias = File('assets/vias.json');
+    Map<String, dynamic> oVias = jsonDecode(vias.readAsStringSync());
+    return oVias;
+  }
+
+  Future<Map> _readJson() async {
+    final String response = await rootBundle.loadString('assets/vias.json');
+    return await json.decode(response);
+  }
+  readJson() async {
+    Map oVias = await _readJson();
+    return oVias;
+  }
+
+
+  _getDetail(o) {
+    List<Widget> items = [];
+    const colors = {
+      'v': Color.fromARGB(255, 135, 135, 135),
+      'v+': Color.fromARGB(255, 135, 135, 135),
+      '6a': Color.fromARGB(255, 255, 240, 31),
+      '6a+': Color.fromARGB(255, 255, 240, 31),
+      '6b': Color.fromARGB(255, 109, 255, 76),
+      '6b+': Color.fromARGB(255, 109, 255, 76),
+      '6c': Color.fromARGB(255, 91, 225, 255),
+      '6c+': Color.fromARGB(255, 91, 225, 255),
+      '7a': Color.fromARGB(255, 255, 149, 1),
+      '7a+': Color.fromARGB(255, 255, 149, 1),
+      '7b': Color.fromARGB(255, 197, 105, 255),
+      '7b+': Color.fromARGB(255, 197, 105, 255),
+      '7c': Color.fromARGB(255, 252, 64, 64),
+      '7c+': Color.fromARGB(255, 252, 64, 64),
+      '8a': Color.fromARGB(255, 50, 50, 50),
+      '8a+': Color.fromARGB(255, 50, 50, 50),
+      '8b': Color.fromARGB(255, 0, 0, 0),
+      '8b+': Color.fromARGB(255, 0, 0, 0),
+      '8c': Color.fromARGB(255, 0, 0, 0),
+      '8c+': Color.fromARGB(255, 0, 0, 0),
+    };
+
+    o.forEach((via) => 
+      items.add(
+        ListTile( 
+          leading: Text(via['grade'], style: TextStyle(fontSize: 18, color: colors[via['grade']])),
+          title: Text(via['name']),
+          subtitle: Text(via['owner']),
+          trailing: Icon(Icons.info),
+          onTap: () {
+            print(via['value']);
+          }
+        )
+      )
+    );
+    return items;
+  }
  
   @override
   Widget build(BuildContext context) {
+    var oVias = _getVias();
+
+    /*oViasFuture.then((oVias) {
+      
+    });*/
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Hellboard'),
@@ -34,88 +100,19 @@ class MyHomePage extends StatelessWidget {
             children: <Widget>[
                 ExpansionTile(
                     title: Text("V+ (Sako patatas)"),
-                    children: <Widget>[
-                        ListTile(
-                          leading: Text("V+", style: TextStyle(fontSize: 18, color: Colors.black)),
-                          title: Text("Erreztxu bet danontzako"),
-                          subtitle: Text("Erik"),
-                        ),
-                        ListTile(
-                          leading: Text("V+", style: TextStyle(fontSize: 18, color: Colors.black)),
-                          title: Text("Benetako kinto masa"),
-                          subtitle: Text("Erik"),
-                        ),
-                    ],
+                    children: _getDetail(oVias['vias']['v'])
                 ),
                 ExpansionTile(
                     title: Text("6 gradue (Hasi entrenaten)"),
-                    children: <Widget>[
-                        ListTile(
-                          leading: Text("6a", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 255, 243, 74))),
-                          title: Text("Erreztxu bet danontzako"),
-                          subtitle: Text("Erik"),
-                        ),
-                        ListTile(
-                          leading: Text("6a+", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 237, 213, 0))),
-                          title: Text("Erreztxu bet danontzako"),
-                          subtitle: Text("Erik"),
-                        ),
-                        ListTile(
-                          leading: Text("6b", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 109, 255, 76))),
-                          title: Text("Benetako kinto masa"),
-                          subtitle: Text("Erik"),
-                        ),
-                        ListTile(
-                          leading: Text("6b+", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 1, 127, 20))),
-                          title: Text("Benetako kinto masa"),
-                          subtitle: Text("Erik"),
-                        ),
-                        ListTile(
-                          leading: Text("6c", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 91, 225, 255))),
-                          title: Text("Benetako kinto masa"),
-                          subtitle: Text("Erik"),
-                        ),
-                        ListTile(
-                          leading: Text("6c+", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 0, 200, 255))),
-                          title: Text("Benetako kinto masa"),
-                          subtitle: Text("Erik"),
-                        ),
-                    ],
+                    children: _getDetail(oVias['vias']['vi']),
                 ),
                 ExpansionTile(
                     title: Text("7 gradue (Mas fuerte que el vinagre)"),
-                    children: <Widget>[
-                        ListTile(
-                          leading: Text("7a", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 255, 190, 91))),
-                          title: Text("Erreztxu bet danontzako"),
-                          subtitle: Text("Erik"),
-                        ),
-                        ListTile(
-                          leading: Text("7a+", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 255, 149, 1))),
-                          title: Text("Erreztxu bet danontzako"),
-                          subtitle: Text("Erik"),
-                        ),
-                        ListTile(
-                          leading: Text("7b", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 197, 105, 255))),
-                          title: Text("Benetako kinto masa"),
-                          subtitle: Text("Erik"),
-                        ),
-                        ListTile(
-                          leading: Text("7b+", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 235, 11, 255))),
-                          title: Text("Benetako kinto masa"),
-                          subtitle: Text("Erik"),
-                        ),
-                        ListTile(
-                          leading: Text("7c", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 252, 64, 64))),
-                          title: Text("Benetako kinto masa"),
-                          subtitle: Text("Erik"),
-                        ),
-                        ListTile(
-                          leading: Text("7c+", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 156, 0, 0))),
-                          title: Text("Benetako kinto masa"),
-                          subtitle: Text("Erik"),
-                        ),
-                    ],
+                    children: _getDetail(oVias['vias']['vii']),
+                ),
+                ExpansionTile(
+                    title: Text("8 (Semidios)"),
+                    children: _getDetail(oVias['vias']['viii']),
                 ),
                
             ],
